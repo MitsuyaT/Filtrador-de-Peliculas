21c0136690f2eef4418d0bb398996259d14e296c
MitsuyaT <lautyrodriguez25@gmail.com>
2024-07-17 11:26:07 -0300

    mapa actualizado para pelis

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const funcion = require("./pelis.js"); //Traemos las funciones al index
+
 function parsear(argv) {
   //Funcion para pasar un array con argumentos a un objeto
   const respuesta = []; //Prepara el array vacio para luego darlo como respuesta
@@ -11,12 +13,23 @@ function parsear(argv) {
   return respuesta; // retornamos todo lo que hicimos
 }
 
+function ejecutarOperacion(filtrador) {
+  const mapa = {
+    "": funcion.sinParametros,
+    sort: funcion.sort,
+    search: funcion.search,
+    tag: funcion.tag,
+  };
+  const filtro = filtrador.filtro;
+  const ejecutor = mapa[filtro];
 
+  return ejecutor();
+}
 
 function main() {
   const soloArgumentos = process.argv.slice(2);
   const argumentoParseado = parsear(soloArgumentos);
-  
+  const resultado = ejecutarOperacion(argumentoParseado);
 }
 
 main();
