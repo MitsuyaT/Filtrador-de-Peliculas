069b6b9fb8a6d3106cee17ff277936f5822f8f5f
Taglianini <tomastagliani1234@gmail.com>
2024-07-17 22:46:06 -0300

    Funcion sort

--- pelis.js
+++ pelis.js
@@ -1,15 +1,24 @@
 const peliculas = require("./pelis.json");
 
-function sinParametros() {
-  return peliculas;
-}
+function sinParametros() {}
 
-function sort() {}
+function sort(pelis) {
+    const titles = pelis.map(pelicula => pelicula.title);
+    const ratings = pelis.map(pelicula => pelicula.rating);
+
+    function sortRatings(ratingArray) {
+        return ratingArray.sort((a, b) => b - a);
+    }
+    function sortTitles(titleArray) {
+        return titleArray.sort();
+    }
+    return {
+        sortedTitles: sortTitles(titles),
+        sortedRatings: sortRatings(ratings)
+    };
+}
+sort(peliculas);
 
 function search() {}
 
 function tag() {}
-
-module.exports = {
-  sinParametros,
-};
