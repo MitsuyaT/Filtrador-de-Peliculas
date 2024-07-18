95e56a6cdac7cfb3de38778b911a0b636ae237ce
MitsuyaT <lautyrodriguez25@gmail.com>
2024-07-18 00:13:43 -0300

    agregado de funcion sin parametros

--- pelis.js
+++ pelis.js
@@ -1,23 +1,25 @@
 const peliculas = require("./pelis.json");
 
-function sinParametros() {}
+function sinParametros() {
+  return peliculas;
+}
 
 function sort(sort) {
-    switch (sort) {
-      case "rating":
-        const ratings = peliculas.map((pelicula) => pelicula.rating);
-        return ratings.sort((a, b) => b - a);
-  
-      case "title":
-        const titles = peliculas.map((pelicula) => pelicula.title);
-        return titles.sort();
-  
-      default:
-        console.log("Error");
-        break;
-    }
+  switch (sort) {
+    case "rating":
+      const ratings = peliculas.map((pelicula) => pelicula.rating);
+      return ratings.sort((a, b) => b - a);
+
+    case "title":
+      const titles = peliculas.map((pelicula) => pelicula.title);
+      return titles.sort();
+
+    default:
+      console.log("Error");
+      break;
   }
-  
+}
+
 function search() {}
 
 function tag(buscaTag) {
